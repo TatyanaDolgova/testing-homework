9a19bbcc9f45a038fedd9147d8711cf83e31dc5b
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-07-03 23:32:06 +0500

    feat: added test for cartBadge

--- test/unit/Catalog.test.tsx
+++ test/unit/Catalog.test.tsx
@@ -7,23 +7,43 @@ import {
   within,
 } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { AxiosResponse } from "axios";
 import { Application } from "../../src/client/Application";
 import { initStore } from "../../src/client/store";
 import { CartApi, ExampleApi } from "../../src/client/api";
-import { CartState, ProductShortInfo } from "../../src/common/types";
+import { CartState, Product, ProductShortInfo } from "../../src/common/types";
 import events from "@testing-library/user-event";
+import { ProductDetails } from "../../src/client/components/ProductDetails";
 
-const mockProducts: ProductShortInfo[] = [
+const mockProductsShortInfo: ProductShortInfo[] = [
   { id: 1, name: "Product 1", price: 1 },
   { id: 2, name: "Product 2", price: 2 },
+];
+
+const mockProducts: Product[] = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 1,
+    description: "Description 1",
+    material: "Material 1",
+    color: "Color 1",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 2,
+    description: "Description 2",
+    material: "Material 2",
+    color: "Color 2",
+  },
 ];
 
 class MockApi extends ExampleApi {
   async getProducts(): Promise<AxiosResponse<ProductShortInfo[]>> {
     return Promise.resolve({
-      data: mockProducts,
+      data: mockProductsShortInfo,
       status: 200,
       statusText: "OK",
       headers: {},
@@ -58,7 +78,7 @@ describe("Каталог", () => {
 
     const productElements = container.querySelectorAll(".ProductItem");
 
-    mockProducts.forEach((product, index) => {
+    mockProductsShortInfo.forEach((product, index) => {
       const productElement = productElements[index];
       if (productElement instanceof HTMLElement) {
         expect(within(productElement).getByText(product.name));
@@ -66,5 +86,35 @@ describe("Каталог", () => {
         expect(within(productElement).getByRole("link", { name: "Details" }));
       }
     });
+  });
+  it("если товар уже добавлен в корзину, на странице каталога должно отображаться сообщение об этом", async () => {
+    const api = new MockApi("");
+    const cartApi = new MockCartApi();
+    const store = initStore(api, cartApi);
+
+    render(
+      <MemoryRouter initialEntries={["/product/1"]}>
+        <Provider store={store}>
+          <Routes>
+            <Route
+              path="/product/:id"
+              element={<ProductDetails product={mockProducts[0]} />}
+            />
+          </Routes>
+        </Provider>
+      </MemoryRouter>
+    );
+
+    await events.click(screen.getByRole("button", { name: "Add to Cart" }));
+
+    render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <Application />
+        </Provider>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("Item in cart"));
   });
 });
--- test/unit/Products.test.tsx
+++ test/unit/Products.test.tsx
@@ -1,19 +1,14 @@
 import React from "react";
-import {
-  fireEvent,
-  render,
-  screen,
-  waitFor,
-  within,
-} from "@testing-library/react";
-import { Provider } from "react-redux";
-import { MemoryRouter } from "react-router-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import { Provider, useSelector } from "react-redux";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { AxiosResponse } from "axios";
 import { Application } from "../../src/client/Application";
-import { initStore } from "../../src/client/store";
+import { ApplicationState, initStore } from "../../src/client/store";
 import { CartApi, ExampleApi } from "../../src/client/api";
 import { CartState, Product, ProductShortInfo } from "../../src/common/types";
 import events from "@testing-library/user-event";
+import { ProductDetails } from "../../src/client/components/ProductDetails";
 
 const mockProductsShortInfo: ProductShortInfo[] = [
   { id: 1, name: "Product 1", price: 1 },
@@ -96,5 +91,27 @@ describe("Тесты для страницы продуктов", () => {
         });
       }
     }
+  });
+
+  it("если товар уже добавлен в корзину, на странице товара должно отображаться сообщение об этом", async () => {
+    const api = new MockApi("");
+    const cartApi = new MockCartApi();
+    const store = initStore(api, cartApi);
+
+    render(
+      <MemoryRouter initialEntries={["/product/1"]}>
+        <Provider store={store}>
+          <Routes>
+            <Route
+              path="/product/:id"
+              element={<ProductDetails product={mockProducts[0]} />}
+            />
+          </Routes>
+        </Provider>
+      </MemoryRouter>
+    );
+
+    await events.click(screen.getByRole("button", { name: "Add to Cart" }));
+    expect(screen.getByText("Item in cart"));
   });
 });
